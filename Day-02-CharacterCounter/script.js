632c7a6e3f1c9a082c0b81f5e29caa1a36384ecd
27jyoti-pixel <raydeepak2711@gmail.com>
2026-08-12 13:47:07 +0530

    half functionality completed

--- Day-02-CharacterCounter/script.js
+++ Day-02-CharacterCounter/script.js
@@ -2,9 +2,13 @@ let user_input = document.querySelector(".user-input")
 let clear = document.querySelector(".but")
 let count = document.querySelector(".count")
 let left_count = document.querySelector(".left-count")
+let alert = document.querySelector(".alert")
 
 user_input.addEventListener("input",()=>{
-  let c = Number((user_input.value).length())
+  let c = (user_input.value).length
+  if(c>79){
+
+  }
   count.textContent=c
   left_count.textContent=100-c
 })
