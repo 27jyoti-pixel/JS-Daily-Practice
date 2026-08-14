f8b321cd2f7119495740f7e06d4413cf627d0cf1
27jyoti-pixel <raydeepak2711@gmail.com>
2026-08-14 16:33:36 +0530

    Day 03: Add character progress bar

--- Day-02-CharacterCounter/script.js
+++ Day-02-CharacterCounter/script.js
@@ -4,25 +4,34 @@ let count = document.querySelector(".count")
 let left_count = document.querySelector(".left-count")
 let alertstat = document.querySelector(".alertstat")
 let stat = document.querySelector(".stat")
+let progress = document.querySelector(".progress-bar")
 
 user_input.addEventListener("input",()=>{
   let c = (user_input.value).length
+  let per = (c/100)*100
+
   if(c>=0 && c<=79){
     alertstat.textContent="You're good to go"
     stat.style.backgroundColor="green"
     count.textContent=c
     left_count.textContent=100-c
+    progress.style.backgroundColor="green"
+    progress.style.width=per+"%"
   }
   else if(c>=80 && c<=99){
     alertstat.textContent="Almost at the limit!"
     stat.style.backgroundColor = "orange"
     count.textContent=c
     left_count.textContent=100-c  
+    progress.style.backgroundColor="green"
+    progress.style.width=per+"%"
   }else if(c>99 && c<=100){
     alertstat.textContent="Character limit reached!"
     stat.style.backgroundColor = "red"
     count.textContent=c
     left_count.textContent=100-c
+    progress.style.backgroundColor="green"
+    progress.style.width=per+"%"
   }
 })
 
@@ -32,4 +41,5 @@ clear.addEventListener("click",()=>{
   left_count.textContent=100
   stat.style.backgroundColor="green"
   alertstat.textContent="You're good to go"
+  progress.style.width=0+"%"
 })
