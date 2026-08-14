8a92c76130830c5124238fc3e2d29c97b424bc2c
27jyoti-pixel <raydeepak2711@gmail.com>
2026-08-14 16:42:47 +0530

    Task 4 completed

--- Day-02-CharacterCounter/script.js
+++ Day-02-CharacterCounter/script.js
@@ -5,6 +5,7 @@ let left_count = document.querySelector(".left-count")
 let alertstat = document.querySelector(".alertstat")
 let stat = document.querySelector(".stat")
 let progress = document.querySelector(".progress-bar")
+let parentprog = document.querySelector(".progress")
 
 user_input.addEventListener("input",()=>{
   let c = (user_input.value).length
@@ -17,21 +18,26 @@ user_input.addEventListener("input",()=>{
     left_count.textContent=100-c
     progress.style.backgroundColor="green"
     progress.style.width=per+"%"
+    parentprog.style.border="2px solid green"
   }
   else if(c>=80 && c<=99){
     alertstat.textContent="Almost at the limit!"
     stat.style.backgroundColor = "orange"
     count.textContent=c
     left_count.textContent=100-c  
-    progress.style.backgroundColor="green"
+    progress.style.backgroundColor="orange"
+    progress.style.borderColor="orange"
     progress.style.width=per+"%"
+    parentprog.style.border="2px solid orange"
   }else if(c>99 && c<=100){
     alertstat.textContent="Character limit reached!"
     stat.style.backgroundColor = "red"
     count.textContent=c
     left_count.textContent=100-c
-    progress.style.backgroundColor="green"
+    progress.style.backgroundColor="red"
+    progress.style.borderColor="red"
     progress.style.width=per+"%"
+    parentprog.style.border="2px solid red"
   }
 })
 
@@ -42,4 +48,5 @@ clear.addEventListener("click",()=>{
   stat.style.backgroundColor="green"
   alertstat.textContent="You're good to go"
   progress.style.width=0+"%"
+  parentprog.style.border="2px solid green"
 })
